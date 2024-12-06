5250c954f6a652ca3893658baf7473ad0b8d7b79
kentRPG <kent00rpg@gmail.com>
2024-12-07 00:17:41 +0800

    refactor(auth): update import paths after renaming components
    - 將 RegisterForm 和 SigninForm 元件重新命名為 Register 和 Signin
    - 更新 auth.tsx 中的 import 路徑以適應新的元件名稱

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import RegisterForm from "@/components/pages/auth/RegisterForm";
-import SigninForm from "@/components/pages/auth/SigninForm";
+import RegisterForm from "@/components/pages/auth/Register";
+import SigninForm from "@/components/pages/auth/Signin";
 
 export default function auth() {
   return (

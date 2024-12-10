10d2d54a17080a9f49653677a5d8a0934e30dc5b
boyi <seraphimboyi@users.noreply.github.com>
2024-12-10 10:35:30 +0800

    feat(user): add  SideBar styles -新增Button的Styled-Component樣式

--- components/pages/user/SideBar/index.tsx
+++ components/pages/user/SideBar/index.tsx
@@ -1,4 +1,4 @@
-import { SideBarContainer, SideBarTabs } from "./styled";
+import { SideBarButton, SideBarContainer, SideBarTabs,SideBarActiveButton } from "./styled";
 import Image from "next/image";
 
 const SideBar = () => {
@@ -7,11 +7,11 @@ const SideBar = () => {
       <SideBarContainer>
         <Image src="/images/Avatar.png" alt="" width={60} height={60} />
         <div>王小姐</div>
-        <div>sk2377328815@gmail.com</div>
+        <div>A0912345678@gmail.com</div>
         <SideBarTabs>
-          <button>基本資料</button>
-          <button>我的訂單</button>
-          <button>詢問單</button>
+          <SideBarActiveButton>基本資料</SideBarActiveButton>
+          <SideBarButton>我的訂單</SideBarButton>
+          <SideBarButton>詢問單</SideBarButton>
         </SideBarTabs>
       </SideBarContainer>
     </>
--- components/pages/user/SideBar/styled.tsx
+++ components/pages/user/SideBar/styled.tsx
@@ -7,7 +7,30 @@ export const SideBarContainer = styled.div`
   padding: 60px 0px;
 `;
 
+// export SideBarHeader = styled.div`
+
+// `
+
 export const SideBarTabs = styled.div`
+  max-width: 204px;
   display: flex;
   flex-direction: column;
+  row-gap: 20px;
+`;
+
+export const SideBarActiveButton = styled.button`
+  width: 100%;
+  font-size: 14px;
+  padding: 10px 0px;
+  background-color: #103f99;
+  border-radius: 10px;
+  color: white;
+`;
+export const SideBarButton = styled.button`
+  width: 100%;
+  font-size: 14px;
+  padding: 10px 0px;
+  background-color: white;
+  border-radius: 10px;
+  color: black;
 `;

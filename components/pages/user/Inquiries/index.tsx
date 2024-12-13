d7c69ce6d7c0a73af0c0cb98ca8de84e5e934c39
boyi <seraphimboyi@users.noreply.github.com>
2024-12-13 13:07:51 +0800

    feat(user):  add Table structure and corresponding styled components
    - added HTML table structure for Inquiries page
    - implemented styled components for Table and related elements

--- components/pages/user/Inquiries/index.tsx
+++ components/pages/user/Inquiries/index.tsx
@@ -1,5 +1,163 @@
+import {
+  CreationDate,
+  InquiryContainer,
+  InquiryLink,
+  InquiryTitle,
+  NumberHeader,
+  OthersHeader,
+  ProductId,
+  Products,
+  ProductsHeader,
+  Status,
+  SuggestionLink,
+  Table,
+  Tbody,
+  Thead,
+  TableHeadTr,
+  TableBodyTr,
+} from "./styled";
+import { MdFileOpen } from "react-icons/md";
+
 const Inquiries = () => {
-  return <></>;
+  return (
+    <>
+      <InquiryContainer>
+        <InquiryTitle>詢問單</InquiryTitle>
+        <Table>
+          <Thead>
+            <TableHeadTr>
+              <NumberHeader>單號</NumberHeader>
+              <ProductsHeader>詢問輔具</ProductsHeader>
+              <OthersHeader>建立日期</OthersHeader>
+              <OthersHeader>狀態</OthersHeader>
+              <OthersHeader>詢問單</OthersHeader>
+              <OthersHeader>建議單</OthersHeader>
+            </TableHeadTr>
+          </Thead>
+
+          <Tbody>
+            <TableBodyTr>
+              <ProductId>AKC833</ProductId>
+              <Products>
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="輪椅"
+                />
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="拐杖"
+                />
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="藍色輪椅"
+                />
+              </Products>
+              <CreationDate>2024/10/04</CreationDate>
+              <Status $isSuggest>
+                <span>店家已評估</span>
+              </Status>
+              <InquiryLink>
+                <button>
+                  <MdFileOpen size={30} color="#103F99" />
+                </button>
+              </InquiryLink>
+              <SuggestionLink>
+                <button>
+                  <MdFileOpen size={30} color="#103F99" />
+                </button>
+              </SuggestionLink>
+            </TableBodyTr>
+          </Tbody>
+          <Tbody>
+            <TableBodyTr>
+              <ProductId>KDO694</ProductId>
+              <Products>
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="輪椅"
+                />
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="拐杖"
+                />
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="藍色輪椅"
+                />
+              </Products>
+              <CreationDate>2024/11/04</CreationDate>
+              <Status>
+                <span>店家評估中</span>
+              </Status>
+              <InquiryLink>
+                <button>
+                  <MdFileOpen size={30} color="#103F99" />
+                </button>
+              </InquiryLink>
+              <SuggestionLink>
+                <button>
+                  <MdFileOpen size={30} color="#103F99" />
+                </button>
+              </SuggestionLink>
+            </TableBodyTr>
+          </Tbody>
+          <Tbody>
+            <TableBodyTr>
+              <ProductId>NKD836</ProductId>
+              <Products>
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="輪椅"
+                />
+                <img
+                  width={80}
+                  height={80}
+                  className="InquiryItemImage"
+                  src="https://shoplineimg.com/5aa0e7330e64fed450000634/5e9530a47692440039880802/800x.jpg?"
+                  alt="拐杖"
+                />
+              </Products>
+              <CreationDate>2024/12/04</CreationDate>
+              <Status>
+                <span>店家評估中</span>
+              </Status>
+              <InquiryLink>
+                <button>
+                  <MdFileOpen size={30} color="#103F99" />
+                </button>
+              </InquiryLink>
+              <SuggestionLink>
+                <button>
+                  <MdFileOpen size={30} color="#103F99" />
+                </button>
+              </SuggestionLink>
+            </TableBodyTr>
+          </Tbody>
+        </Table>
+      </InquiryContainer>
+    </>
+  );
 };
 
 export default Inquiries;
---
+++ components/pages/user/Inquiries/styled.tsx
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+interface ButtonProps {
+  $isSuggest?: boolean;
+}
+
+export const InquiryContainer = styled.div`
+  flex: 1;
+  padding: 48px;
+  border-radius: 10px;
+  outline: 1px #888888 solid;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
+`;
+
+export const InquiryTitle = styled.h4`
+  font-size: 24px;
+  color: #08204d;
+  height: 36px;
+  border-left: 5px #103f99 solid;
+  padding: 0px 14px;
+  margin-bottom: 24px;
+`;
+
+export const Table = styled.table`
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  outline: 1px #888888 solid;
+`;
+export const Thead = styled.thead`
+  background-color: #cfd9eb;
+  padding: 10px 25px;
+  font-size: 12px;
+  color: #08204d;
+`;
+export const TableHeadTr = styled.tr`
+  display: flex;
+  column-gap: 16px;
+`;
+export const NumberHeader = styled.th`
+  min-width: 126px;
+`;
+
+export const ProductsHeader = styled.th`
+  min-width: 290px;
+`;
+
+export const OthersHeader = styled.th`
+  min-width: 90px;
+`;
+
+export const Tbody = styled.tbody`
+  padding: 24px 25px;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 1px; /* 框線寬度 */
+    background-color: #ccc; /* 框線顏色 */
+  }
+  &:last-of-type::after {
+    content: none;
+  }
+`;
+
+export const TableBodyTr = styled.tr`
+  display: flex;
+  column-gap: 16px;
+`;
+
+export const ProductId = styled.td`
+  min-width: 126px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Products = styled.td`
+  min-width: 290px;
+  display: flex;
+`;
+
+export const CreationDate = styled.td`
+  min-width: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Status = styled.td<ButtonProps>`
+  min-width: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ $isSuggest }) => ($isSuggest ? "#2ECC71" : "#E74C3C")};
+`;
+
+export const InquiryLink = styled.td`
+  min-width: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const SuggestionLink = styled.td`
+  min-width: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

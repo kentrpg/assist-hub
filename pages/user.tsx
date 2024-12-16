0d8cc53c33711f28bc33cc18ceb023c3b8e54f10
boyi <seraphimboyi@users.noreply.github.com>
2024-12-16 12:23:55 +0800

    refactor(user): update wrapper structure and import paths
    - Replaced `UserWrapper` with `Wrapper` for improved naming consistency
    - Cleaned up and reordered import paths for better readability
    - Updated the `user` component to align with refactored styles

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import Profile from "@/components/pages/user/Profile";
+import { MainWrapper } from "@/styles/wrappers";
+import Wrapper from "@/components/pages/user/Wrapper/index";
 import SideBar from "@/components/pages/user/SideBar";
-// import { MainWrapper } from "@/styles/wrappers";
-import UserWrapper from "@/components/pages/user/Wrapper/index";
+import Profile from "@/components/pages/user/Profile";
 import Orders from "@/components/pages/user/Orders";
 import Inquiries from "@/components/pages/user/Inquiries";
-import { MainWrapper } from "@/styles/wrappers";
 
 const User = () => {
   const [activeTab, setActiveTab] = useState<
@@ -15,12 +14,12 @@ const User = () => {
   return (
     <>
       <MainWrapper>
-        <UserWrapper>
+        <Wrapper>
           <SideBar setActiveTab={setActiveTab} activeTab={activeTab} />
           {activeTab === "profile" && <Profile />}
           {activeTab === "orders" && <Orders />}
           {activeTab === "inquiries" && <Inquiries />}
-        </UserWrapper>
+        </Wrapper>
       </MainWrapper>
     </>
   );

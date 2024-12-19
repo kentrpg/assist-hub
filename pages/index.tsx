6d3dca7de31a3b7c682abc9f73beb0cccbeac3ac
kentRPG <kent00rpg@gmail.com>
2024-12-19 12:12:19 +0800

    fix(img): resolve image path error
    - 修正 chairman.svg 圖片路徑錯誤
    - 加入前導斜線確保從根目錄引用

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div style={{ textAlign: "center", transform: "scale(0.5)" }}>
-        <img src="images/chairman.svg" />
+        <img src="/images/chairman.svg" />
       </div>
     </>
   );

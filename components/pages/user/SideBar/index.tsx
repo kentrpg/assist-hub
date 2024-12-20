84ac4a92c0a141287e2268bc617fa897f4d3859e
boyi <seraphimboyi@users.noreply.github.com>
2024-12-20 16:46:44 +0800

    refactor(sidebar): restructure component and extract data logic
    - 將 SideBar 的資料邏輯拆分到獨立的 `data.ts` 文件
    - 優化 SideBar 的結構與邏輯，提升可讀性與模組化程度
    - 簡化 props 傳遞與狀態管理

--- components/pages/user/SideBar/index.tsx
+++ components/pages/user/SideBar/index.tsx
@@ -7,26 +7,22 @@ import {
   Tabs,
   Tab,
   Profile,
-  Basic,
-  Orders,
-  Inquiries,
 } from "./styled";
+import { tabs, ActiveTabType } from "./data";
 import Image from "next/image";
-import { MdPerson, MdChecklist, MdOutlineAccessible } from "react-icons/md";
 import React from "react";
 
-// 定義 props 的類型
-interface SideBarProps {
-  setActiveTab: (tab: "profile" | "orders" | "inquiries") => void; // 僅限三個可能值
-  activeTab: "profile" | "orders" | "inquiries";
-}
+type SideBarProps = {
+  setActiveTab: (tab: ActiveTabType) => void; //通常用在FC上(只處理東西)
+  activeTab: ActiveTabType;
+};
 
 const SideBar: React.FC<SideBarProps> = ({ setActiveTab, activeTab }) => {
   return (
     <SideBarContainer>
       <Profile>
         <Image
-          src="/images/Avatar.png"
+          src="/images/Profile.svg"
           alt="用戶頭像"
           width={60}
           height={60}
@@ -37,34 +33,18 @@ const SideBar: React.FC<SideBarProps> = ({ setActiveTab, activeTab }) => {
         <Name>王小姐</Name>
         <Email>A0912345678@gmail.com</Email>
       </Info>
+
       <Tabs>
-        <Tab
-          $isActive={activeTab === "profile"}
-          onClick={() => setActiveTab("profile")}
-        >
-          <Basic>
-            <MdPerson size={24} />
-            <Span>基本資料</Span>
-          </Basic>
-        </Tab>
-        <Tab
-          $isActive={activeTab === "orders"}
-          onClick={() => setActiveTab("orders")}
-        >
-          <Orders>
-            <MdChecklist size={24} />
-            <Span>我的訂單</Span>
-          </Orders>
-        </Tab>
-        <Tab
-          $isActive={activeTab === "inquiries"}
-          onClick={() => setActiveTab("inquiries")}
-        >
-          <Inquiries>
-            <MdOutlineAccessible size={24} />
-            <Span>詢問單</Span>
-          </Inquiries>
-        </Tab>
+        {tabs.map((tab) => (
+          <Tab
+            key={tab.key}
+            $isActive={tab.isActive(activeTab)}
+            onClick={() => setActiveTab(tab.key)}
+          >
+            {tab.icon}
+            <Span>{tab.label}</Span>
+          </Tab>
+        ))}
       </Tabs>
     </SideBarContainer>
   );

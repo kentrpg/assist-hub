e22d58685cbe874e4ea2658ca30b18c681217ad9
boyi <seraphimboyi@users.noreply.github.com>
2024-12-20 16:44:03 +0800

    refactor(user): enhance tab navigation and integrate order details
    - 新增 `selectedOrder` 狀態，用於管理選擇的訂單
    - 在 `Orders` 中加入 `setActiveOrder` 邏輯，實現與 `Details` 頁面的動態切換
    - 調整 `SideBar` 和 `Orders` 的 props 傳遞與狀態管理
    - 簡化 tab 導航邏輯，提升代碼可讀性與維護性

--- pages/user.tsx
+++ pages/user.tsx
@@ -5,23 +5,33 @@ import SideBar from "@/components/pages/user/SideBar";
 import Profile from "@/components/pages/user/Profile";
 import Orders from "@/components/pages/user/Orders";
 import Inquiries from "@/components/pages/user/Inquiries";
+import Details from "@/components/pages/user/Orders/Details";
+import { Order } from "../components/pages/user/Orders/data";
+import { ActiveTabType } from "@/components/pages/user/SideBar/data";
 
 const User = () => {
-  const [activeTab, setActiveTab] = useState<
-    "profile" | "orders" | "inquiries"
-  >("profile");
+  const [activeTab, setActiveTab] = useState<ActiveTabType>("profile");
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
 
   return (
-    <>
-      <MainWrapper>
-        <Wrapper>
-          <SideBar setActiveTab={setActiveTab} activeTab={activeTab} />
-          {activeTab === "profile" && <Profile />}
-          {activeTab === "orders" && <Orders />}
-          {activeTab === "inquiries" && <Inquiries />}
-        </Wrapper>
-      </MainWrapper>
-    </>
+    <MainWrapper>
+      <Wrapper>
+        <SideBar setActiveTab={setActiveTab} activeTab={activeTab} />
+        {activeTab === "profile" && <Profile />}
+        {activeTab === "orders" && (
+          <Orders
+            setActiveOrder={(order) => {
+              setSelectedOrder(order);
+              setActiveTab("details");
+            }}
+          />
+        )}
+        {activeTab === "details" && selectedOrder && (
+          <Details order={selectedOrder} />
+        )}
+        {activeTab === "inquiries" && <Inquiries />}
+      </Wrapper>
+    </MainWrapper>
   );
 };
 

5f939112aa016b8dab2ef0e5f149496312d449eb
boyi <seraphimboyi@users.noreply.github.com>
2024-12-27 12:28:34 +0800

    update: modify theme configuration
    - 更新 `theme.tsx` 中的樣式設定。
    - 調整全局樣式以適配最新的設計需求。

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,7 +1,7 @@
 const colors = {
   primary: "#103F99",
   primaryBg: "#FFFFFF",
-  primaryLight: "#CFD9EB",
+  primaryLight: "#E7ECF5",
   textprimary: "#08204D",
   secondary: "#FFC123",
   seccondaryLight: "#FFF3D3",
@@ -16,6 +16,8 @@ const colors = {
   success: "#2ECC71",
   border: "#E9E5DE",
   info: "#2F80ED",
+  lineLogo: "#06c755",
+  fbLogo: "#3b579d",
   gray: {
     100: "#E9E5DE",
     200: "#B3B3B3",

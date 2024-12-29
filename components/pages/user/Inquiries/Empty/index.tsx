ffcfed81b3bc828afdd618e7faae6b05f345e682
boyi <seraphimboyi@users.noreply.github.com>
2024-12-29 11:29:46 +0800

    feat(user): add Empty component for Inquiries page and update Orders page
    - 新增 `Empty` 元件及樣式至 `Inquiries` 頁面。
    - 更新 `Orders` 頁面的 `Empty` 元件，調整圖片引入路徑。

--- components/pages/user/Inquiries/Empty/index.tsx
+++ components/pages/user/Inquiries/Empty/index.tsx
@@ -5,7 +5,7 @@ const Empty = () => {
   return (
     <Container>
       <img src="/images/Empty.svg" alt="" width={280} height={280} />
-      <Title>尚未有任何訂單</Title>
+      <Title>尚未有任何詢問單</Title>
     </Container>
   );
 };

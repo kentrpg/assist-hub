8c588a8a507e7f1d0998ef8897544132bddf21cc
boyi <seraphimboyi@users.noreply.github.com>
2025-01-11 00:00:18 +0800

    refactor(product): refine filter logic and structure for all products page
    - 完善全部產品頁面的篩選邏輯，實現更精確的產品篩選功能。
    - 更新頁面結構，調整 `index.tsx` 以支持新功能和優化佈局。
    - 更新樣式 (`styled.tsx`)。
    - 為用戶提供更直觀的產品篩選體驗，改善交互效果。

--- components/pages/product/All/index.tsx
+++ components/pages/product/All/index.tsx
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import {
   Container,
   SideBar,
-  FilteredResults,
+  FilterWrapper,
   WheelChair,
   Crutch,
   Bed,
   Oxygen,
   CardWrapper,
-  Img,
+  CardImg,
   Card,
   Title,
   Name,
   Rent,
   Info,
-  Icons,
+  Tabs,
   Type,
-  IconWrapper,
+  TypeWrapper,
+  LevelWrapper,
+  Tab,
+  Radio,
+  Level,
+  Radios,
+  Overlay,
+  Img,
+  ImgWrapper,
 } from "./styled";
 import { ProductItem } from "./data";
+import { MdInfo, MdRadioButtonChecked, MdOutlineCircle } from "react-icons/md";
 
 type AllProps = {
   products: ProductItem[];
@@ -31,33 +41,106 @@ const All: React.FC<AllProps> = ({ products }) => {
   const beds = products.filter((product) => product.type === "bed");
   const oxygens = products.filter((product) => product.type === "oxygen");
 
+  const [selectedLevel, setSelectedLevel] = useState<string>("");
+
+  const handleRadioChange = (value: string) => {
+    if (selectedLevel === value) {
+      setSelectedLevel("");
+    } else {
+      setSelectedLevel(value);
+    }
+  };
   return (
     <Container>
       <SideBar>
-        <Type>輔具類型</Type>
-        <Icons>
-          <IconWrapper>
-            <img src="/images/crutch.png" alt="" />
-          </IconWrapper>
-          <IconWrapper>
-            <img src="/images/bed.png" alt="" />
-          </IconWrapper>
-          <IconWrapper>
-            <img src="/images/oxygen.png" alt="" />
-          </IconWrapper>
-          <IconWrapper>
-            <img src="/images/crutch.png" alt="" />
-          </IconWrapper>
-        </Icons>
+        <TypeWrapper>
+          <Type>輔具類型</Type>
+          <Tabs>
+            <Tab>
+              <ImgWrapper>
+                <Img src="/images/crutch.png" alt="拐杖" />
+              </ImgWrapper>
+              <Overlay>行動輪椅</Overlay>
+            </Tab>
+            <Tab>
+              <ImgWrapper>
+                <Img src="/images/bed.png" alt="床" />
+              </ImgWrapper>
+              <Overlay>柺杖步行</Overlay>
+            </Tab>
+            <Tab>
+              <ImgWrapper>
+                <Img src="/images/oxygen.png" alt="製氧機" />
+              </ImgWrapper>
+              <Overlay>臥室寢具</Overlay>
+            </Tab>
+            <Tab>
+              <ImgWrapper>
+                <Img src="/images/crutch.png" alt="拐杖" />
+              </ImgWrapper>
+              <Overlay>呼吸照護</Overlay>
+            </Tab>
+          </Tabs>
+        </TypeWrapper>
+        <LevelWrapper>
+          <Level>
+            動作功能分級表
+            <MdInfo color="#103F99" size={24} />
+          </Level>
+          <Radios>
+            <Radio onClick={() => handleRadioChange("1")}>
+              {selectedLevel === "1" ? (
+                <MdRadioButtonChecked size={24} color="#103F99" />
+              ) : (
+                <MdOutlineCircle size={24} color="#103F99" />
+              )}
+              <label htmlFor="level1">具平地跑跳能力</label>
+            </Radio>
+            <Radio onClick={() => handleRadioChange("2")}>
+              {selectedLevel === "2" ? (
+                <MdRadioButtonChecked size={24} color="#103F99" />
+              ) : (
+                <MdOutlineCircle size={24} color="#103F99" />
+              )}
+              <label htmlFor="level2">在平地無法跑跳，但可放手行走</label>
+            </Radio>
+            <Radio onClick={() => handleRadioChange("3")}>
+              {selectedLevel === "3" ? (
+                <MdRadioButtonChecked size={24} color="#103F99" />
+              ) : (
+                <MdOutlineCircle size={24} color="#103F99" />
+              )}
+              <label htmlFor="level3">行走需扶持穩定物</label>
+            </Radio>
+            <Radio onClick={() => handleRadioChange("4")}>
+              {selectedLevel === "4" ? (
+                <MdRadioButtonChecked size={24} color="#103F99" />
+              ) : (
+                <MdOutlineCircle size={24} color="#103F99" />
+              )}
+              <label htmlFor="level4">
+                無法行走，但能在無扶桿支撐下維持坐姿
+              </label>
+            </Radio>
+            <Radio onClick={() => handleRadioChange("5")}>
+              {selectedLevel === "5" ? (
+                <MdRadioButtonChecked size={24} color="#103F99" />
+              ) : (
+                <MdOutlineCircle size={24} color="#103F99" />
+              )}
+              <label htmlFor="level5">無頭靠支撐下難以維持坐姿</label>
+            </Radio>
+          </Radios>
+        </LevelWrapper>
       </SideBar>
-      <FilteredResults>
+      <FilterWrapper>
         {/* 行動輪椅 */}
         <WheelChair>
           <Title $color="primary">行動輪椅</Title>
           <CardWrapper>
             {wheelchairs.map((product) => (
               <Card $bg="primaryLight" key={product.id}>
-                <Img
+                <CardImg
                   src="/images/wheelChair.svg"
                   alt={product.imgAlt || product.name}
                 />
@@ -76,7 +159,7 @@ const All: React.FC<AllProps> = ({ products }) => {
           <CardWrapper>
             {crutches.map((product) => (
               <Card $bg="accentLight" key={product.id}>
-                <Img
+                <CardImg
                   src="/images/wheelChair.svg"
                   alt={product.imgAlt || product.name}
                 />
@@ -95,7 +178,7 @@ const All: React.FC<AllProps> = ({ products }) => {
           <CardWrapper>
             {beds.map((product) => (
               <Card $bg="primaryLight" key={product.id}>
-                <Img
+                <CardImg
                   src="/images/wheelChair.svg"
                   alt={product.imgAlt || product.name}
                 />
@@ -110,11 +193,11 @@ const All: React.FC<AllProps> = ({ products }) => {
 
         {/* 製氧機 */}
         <Oxygen>
-          <Title $color="accent">製氧機</Title>
+          <Title $color="accent">呼吸照護</Title>
           <CardWrapper>
             {oxygens.map((product) => (
               <Card $bg="accentLight" key={product.id}>
-                <Img
+                <CardImg
                   src="/images/wheelChair.svg"
                   alt={product.imgAlt || product.name}
                 />
@@ -126,7 +209,7 @@ const All: React.FC<AllProps> = ({ products }) => {
             ))}
           </CardWrapper>
         </Oxygen>
-      </FilteredResults>
+      </FilterWrapper>
     </Container>
   );
 };
--- components/pages/product/All/styled.tsx
+++ components/pages/product/All/styled.tsx
@@ -16,36 +16,117 @@ export const Container = styled(Container1344)`
 `;
 
 export const SideBar = styled.div`
-  padding: 0px 12px;
+  padding: 16px 12px;
   max-width: 366px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 30px;
 `;
 
+export const TypeWrapper = styled.div``;
+export const LevelWrapper = styled.div``;
+
 export const Type = styled.div`
-  padding: 16px 0px;
+  padding-bottom: 16px;
   font-size: 16px;
   font-weight: 500;
   color: #08204d;
 `;
 
-export const Icons = styled.div`
+export const Level = styled.div`
+  padding-bottom: 16px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #08204d;
+  display: flex;
+  column-gap: 8px;
+  align-items: center;
+`;
+
+export const Tabs = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 8px;
   width: 100%;
+  padding: 12px 0px;
 `;
 
-export const IconWrapper = styled.div`
+export const Tab = styled.div`
+  position: relative;
   display: flex;
-  outline: #e9e5de 1px solid;
-  border-radius: 10px;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  outline: #e9e5de 1px solid;
+  border-radius: 10px;
   max-width: 167px;
+  padding-top: 10px 0px;
   width: 100%;
+  overflow: hidden;
+  cursor: pointer;
+
+  &:hover img {
+    width: 33px;
+    height: 33px;
+  }
+
+  &:hover div {
+    transform: translateY(0);
+    opacity: 1;
+  }
 `;
 
-export const FilteredResults = styled.div`
+export const ImgWrapper = styled.div`
+  width: 100%;
+  height: 66px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const Img = styled.img`
+  width: 66px;
+  height: 66px;
+  transition: all 0.3s ease-in-out;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(1); /* 將中心點設為縮放中心 */
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 30%; /* 占容器的下半部分 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #103f99; /* 背景顏色 */
+  color: #fff; /* 字體顏色 */
+  font-size: 16px;
+  font-weight: 700;
+  border-radius: 0 0 10px 10px; /* 下部圓角 */
+  transform: translateY(100%); /* 初始位置完全在下方 */
+  opacity: 0; /* 初始透明 */
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out; /* 平滑動畫效果 */
+`;
+
+export const Radios = styled.div``;
+
+export const Radio = styled.div`
+  padding: 8px 12px;
+  display: flex;
+  align-items: center;
+  column-gap: 6px;
+`;
+
+export const FilterWrapper = styled.div`
+  padding: 16px 12px;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -75,7 +156,7 @@ export const Card = styled.div<CardProps>`
   border-radius: 10px;
 `;
 
-export const Img = styled.img`
+export const CardImg = styled.img`
   width: 100%;
   max-height: 190px;
   margin: 0;
@@ -99,25 +180,21 @@ export const Rent = styled.span`
 `;
 
 export const WheelChair = styled.div`
-  padding: 24px 12px;
   display: flex;
   flex-direction: column;
   row-gap: 16px;
 `;
 export const Crutch = styled.div`
-  padding: 24px 12px;
   display: flex;
   flex-direction: column;
   row-gap: 16px;
 `;
 export const Bed = styled.div`
-  padding: 24px 12px;
   display: flex;
   flex-direction: column;
   row-gap: 16px;
 `;
 export const Oxygen = styled.div`
-  padding: 24px 12px;
   display: flex;
   flex-direction: column;
   row-gap: 16px;

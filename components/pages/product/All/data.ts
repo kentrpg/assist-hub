48a55d5c9fa0de8f31edae15f2291e43f711306d
boyi <seraphimboyi@users.noreply.github.com>
2025-01-20 21:49:28 +0800

    refactor(all): update terminology for crutch
    - 將 all 頁面中資料的 "枴杖" 更新為 "拐杖" 字樣以符合設計稿。

--- components/pages/product/All/data.ts
+++ components/pages/product/All/data.ts
@@ -10,7 +10,7 @@ export type ProductItem = {
 
 export const tabsData = [
   { label: "行動輪椅", value: "wheelChair" },
-  { label: "柺杖步行", value: "crutch" },
+  { label: "拐杖步行", value: "crutch" },
   { label: "臥室寢具", value: "bed" },
   { label: "呼吸照護", value: "oxygen" },
 ];

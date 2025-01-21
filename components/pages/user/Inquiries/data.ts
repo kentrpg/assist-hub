451a2b83336b638e1bc82ebae7bb7bf9e991f54a
boyi <seraphimboyi@users.noreply.github.com>
2025-01-21 14:26:52 +0800

    refactor(inquiry): define inquiry type and remove static data
    - 為 inquiry 詢問單新增詳細的型別定義。
    - 移除靜態資料並改用型別定義以支援動態資料來源。

--- components/pages/user/Inquiries/data.ts
+++ components/pages/user/Inquiries/data.ts
@@ -1,43 +1,13 @@
 export type InquiryData = {
-  id: string;
-  products: string[];
+  inquiryId: number;
+  inquiryCode: string;
   createdDate: string;
-  isSuggest: boolean;
-  status: string;
+  createdStamp: string;
+  isReplied: boolean;
+  images: Array<{
+    src: string;
+    alt: string;
+  }>;
+  suggetsId: number;
+  suggetsCode: string;
 };
-
-export const inquiries: InquiryData[] = [
-  {
-    id: "AKC833",
-    products: [
-      "/images/wheelChair.svg",
-      "/images/wheelChair.svg",
-      "/images/wheelChair.svg",
-    ],
-    createdDate: "2024/10/04",
-    isSuggest: true,
-    status: "已回覆",
-  },
-  {
-    id: "KDO694",
-    products: [
-      "/images/wheelChair.svg",
-      "/images/wheelChair.svg",
-      "/images/wheelChair.svg",
-    ],
-    createdDate: "2024/10/04",
-    isSuggest: false,
-    status: "未回覆",
-  },
-  {
-    id: "NKD836",
-    products: [
-      "/images/wheelChair.svg",
-      "/images/wheelChair.svg",
-      "/images/wheelChair.svg",
-    ],
-    createdDate: "2024/10/04",
-    isSuggest: false,
-    status: "未回覆",
-  },
-];

01922f5d7d6ddebc26485e27759d4630a7db8570
boyi <seraphimboyi@users.noreply.github.com>
2025-02-01 21:44:32 +0800

    style(product): add breadcrumb ellipsis & update inquiry button visibility
    - 為 All 頁面新增麵包屑部分，當文字超過寬度時顯示省略符號，避免內容超出容器
    - 調整商品卡片的詢問單按鈕行為，原本 hover 才顯示，現在在 1024px 以下直接顯示

--- components/pages/product/Single/BreadCrumb/styled.tsx
+++ components/pages/product/Single/BreadCrumb/styled.tsx
@@ -13,11 +13,18 @@ export const Li = styled.li`
   font-size: 16px;
   font-weight: 500;
   color: #103f99;
+  &:last-child {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
   :hover {
     color: #0b2c6b;
   }
   a {
     font-weight: 400;
     color: #888888;
+    white-space: nowrap;
   }
 `;

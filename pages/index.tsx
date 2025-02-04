4f472135e0a404ba479137ec4679c41f9b54c3e3
boyi <seraphimboyi@users.noreply.github.com>
2025-02-04 14:14:42 +0800

    feat(home): add meta title and description for SEO
    - 在 Home 頁面新增 `<Head>`，設定 `title` 為 "RENT4U 輔具租賃網"
    - 增加 `meta description`，描述網站功能，提升 SEO 表現

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import { NextPage } from "next";
 import Home from "@/components/pages/Home";
+import Head from "next/head";
 
 const HomePage: NextPage = () => {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>RENT4U 輔具租賃網</title>
+        <meta
+          name="description"
+          content="讓線上輔具租賃流程更加簡單、直觀，讓用戶無縫接軌從選擇到租賃的每一個步驟。"
+        />
+      </Head>
+      <Home />;
+    </>
+  );
 };
 
 export default HomePage;

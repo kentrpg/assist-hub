91b3283ab3137631716d92dcc0507bc11720463b
kentRPG <kent00rpg@gmail.com>
2025-02-04 21:51:22 +0800

    refactor(footer, hooks): simplify Next.js Link usage
    - 移除 Footer 元件中過時的 Next.js Link 屬性，優化程式碼結構

--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -49,51 +49,35 @@ const Footer: React.FC = () => {
           <Categories>
             <Title>輔具分類</Title>
             <CategoryLinks>
+              <CategoryLink href="/product?type=wheelChair">
+                行動輔椅
+              </CategoryLink>
+              <CategoryLink href="/product?type=oxygen">呼吸照護</CategoryLink>
+              <CategoryLink href="/product?type=crutch">拐杖步行</CategoryLink>
+              <CategoryLink href="/product?type=bed">臥室寢具</CategoryLink>
               <CategoryLink
-                href="/product?type=wheelChair"
-                passHref
-                legacyBehavior
+                href="#"
+                onClick={() => {
+                  return false;
+                }}
               >
-                <a>行動輔椅</a>
-              </CategoryLink>
-              <CategoryLink href="/product?type=oxygen" passHref legacyBehavior>
-                <a>呼吸照護</a>
-              </CategoryLink>
-              <CategoryLink href="/product?type=crutch" passHref legacyBehavior>
-                <a>拐杖步行</a>
-              </CategoryLink>
-              <CategoryLink href="/product?type=bed" passHref legacyBehavior>
-                <a>臥室寢具</a>
+                如廁沐浴
               </CategoryLink>
               <CategoryLink
                 href="#"
                 onClick={() => {
                   return false;
                 }}
-                passHref
-                legacyBehavior
               >
-                <a>如廁沐浴</a>
+                居家照護
               </CategoryLink>
               <CategoryLink
                 href="#"
                 onClick={() => {
                   return false;
                 }}
-                passHref
-                legacyBehavior
               >
-                <a>居家照護</a>
-              </CategoryLink>
-              <CategoryLink
-                href="#"
-                onClick={() => {
-                  return false;
-                }}
-                passHref
-                legacyBehavior
-              >
-                <a>輔具、護具</a>
+                輔具、護具
               </CategoryLink>
             </CategoryLinks>
           </Categories>

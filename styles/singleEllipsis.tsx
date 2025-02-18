d72dade765232f02c948c0e950d24d19e0f99c7f
kentRPG <kent00rpg@gmail.com>
2025-02-18 14:07:41 +0800

    refactor(ui): standardize text ellipsis with dynamic line clamp
    - 更新 `singleEllipsis` 工具，以接受動態行數參數
    - 在 InquiryCard 和 OrderDetails 元件中，將原本的省略樣式替換為新的 `singleEllipsis` css function

--- styles/singleEllipsis.tsx
+++ styles/singleEllipsis.tsx
@@ -1,9 +1,9 @@
 import { css } from "styled-components";
 
-export const singleEllipsis = css`
+export const singleEllipsis = (lines: number = 1) => css`
   display: -webkit-box;
   -webkit-box-orient: vertical;
-  -webkit-line-clamp: 1;
+  -webkit-line-clamp: ${lines};
   overflow: hidden;
   text-overflow: ellipsis;
 `;
